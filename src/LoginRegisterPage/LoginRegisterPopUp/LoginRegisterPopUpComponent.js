4d68e78a4e208f26a0c0d29363c8608abfcd7667
Warw1ck <bradvata15@gmail.com>
2023-11-19 11:37:10 +0200

    Create a close button for popup form

--- src/LoginRegisterPage/LoginRegisterPopUp/LoginRegisterPopUpComponent.js
+++ src/LoginRegisterPage/LoginRegisterPopUp/LoginRegisterPopUpComponent.js
@@ -1,6 +1,8 @@
 import { useRef, useState, useEffect } from "react";
 import LoginFormComponent from "./LoginForm/LoginFormComponent";
 import RegisterFormComponent from "./RegisterForm/RegisterFormComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark} from "@fortawesome/free-solid-svg-icons";
 
 const LoginPopUpComponent = ({setLoginFormPopUp}) => {
   const [formName, setForm] = useState('login')
@@ -26,6 +28,7 @@ const LoginPopUpComponent = ({setLoginFormPopUp}) => {
     <>
       <div className="wrapper fadeInDown">
         <div id="formContent" ref={newRef}>
+        <FontAwesomeIcon className="closeIcon" onClick={()=>setLoginFormPopUp(false)} icon={faCircleXmark} />
           {/* <!-- Tabs Titles --> */}
           <h2 onClick={()=>{setForm('login')}} className={formName === 'login'? 'active':'inactive underlineHover'}>Sign In</h2>
           <h2 onClick={()=>{setForm('register')}} className={formName === 'register'? 'active':'inactive underlineHover'}>Sign Up</h2>
@@ -35,6 +38,7 @@ const LoginPopUpComponent = ({setLoginFormPopUp}) => {
 
           {formName === 'login'&& <LoginFormComponent/>}
           {formName ==='register' && <RegisterFormComponent/>}
+
 
         </div>
       </div>
