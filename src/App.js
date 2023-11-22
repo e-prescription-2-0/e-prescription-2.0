fc5601ab48e284260f6fd92fb3869fd85c34d1fe
Warw1ck <bradvata15@gmail.com>
2023-11-22 20:37:41 +0200

    Create Benefit page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Popup from "./components/Popup/Popup";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import WelcomeView from "./components/WelcomeView/WelcomeView";
+import Benefits from "./components/Benefits/Benefits";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={WelcomeView} />
         <Route path="/popup" component={Popup} />
+        <Route path="/Benefits" component={Benefits}/>
       </Switch>
     </Router>
   );
