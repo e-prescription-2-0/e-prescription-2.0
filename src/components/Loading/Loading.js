a76ba58d4a27cb3474a17d5e1574a4b677b89b56
Warw1ck <bradvata15@gmail.com>
2023-11-28 18:43:33 +0200

    Create Loading"

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,22 @@
+import './Loading.css'
+const Loading = () => {
+  return (
+    <div class="absCenter ">
+      <div class="loaderPill">
+        <div class="loaderPill-anim">
+          <div class="loaderPill-anim-bounce">
+            <div class="loaderPill-anim-flop">
+              <div class="loaderPill-pill"></div>
+            </div>
+          </div>
+        </div>
+        <div class="loaderPill-floor">
+          <div class="loaderPill-floor-shadow"></div>
+        </div>
+        <div class="loaderPill-text">Loading your RX</div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading
