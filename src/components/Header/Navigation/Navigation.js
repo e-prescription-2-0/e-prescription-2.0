eaaa957a3ae1a72192f5e3e6842c077b32887394
Warw1ck <bradvata15@gmail.com>
2023-12-01 18:26:44 +0200

    Make navigation bar more responsive

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,24 +1,24 @@
-import { useState } from "react"
-import { navLinkConfig } from "../../../constants/navigation"
-import style from "./Navigation.module.css"
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { navLinkConfig } from "../../../constants/navigation";
+import style from "./Navigation.module.css";
 
 export const Navigation = () => {
-  const [currentUser, setCurrentUser] = useState("patient")
+  const [currentUser, setCurrentUser] = useState("patient");
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  let user = false
-
-  const [isLoginClick, setLogin] = useState(false)
-
-  const onClick = () => {
-    // setLogin(isLoginClick = !isLoginClick)
-  }
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
-    <nav className={style["navigation"]}>
-      <ul className={style["navigation-list"]}>
+    <nav className={`${style["navigation"]} ${isMobileMenuOpen ? style["open"] : ""}`}>
+      <FontAwesomeIcon icon={faBars} className={style["mobile-menu-icon"]} onClick={toggleMobileMenu} />
+      <ul className={`${style["navigation-list"]} ${isMobileMenuOpen ? style["open"] : ""}`}>
         {navLinkConfig.map((link) =>
           !link.hideFor.includes(currentUser) ? (
-            <li className={style["navigation-list-item"]}>
+            <li className={style["navigation-list-item"]} key={link.linkName}>
               <a className={style["a"]} onClick={link.onClick}>
                 {link.linkName}
               </a>
@@ -27,5 +27,5 @@ export const Navigation = () => {
         )}
       </ul>
     </nav>
-  )
-}
+  );
+};
