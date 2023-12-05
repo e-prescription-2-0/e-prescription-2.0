b26e1a0ac6483c328981cc7612f0ec12e89e2a98
Luben Sirakov <lubo95sirakov@gmail.com>
2023-12-05 20:48:08 +0200

    cr fix

--- src/components/Welcome/Passion/Passion.js
+++ src/components/Welcome/Passion/Passion.js
@@ -1,10 +1,10 @@
-import Carousel from "react-bootstrap/Carousel";
-import style from "./Passion.module.css";
+import Carousel from "react-bootstrap/Carousel"
+import style from "./Passion.module.css"
 
+import PassionCard from "./PassionCard"
 import doctorImage from "./passion-images/Becoming-a-medical-doctor-or-physician-in-Germany.jpg"
-import pharmacistImage from "./passion-images/pexels-edward-jenner-4031417.jpg";
-import doctorAndPatientImage from "./passion-images/pexels-cottonbro-studio-7579831.jpg";
-import PassionCard from "./PassionCard";
+import doctorAndPatientImage from "./passion-images/pexels-cottonbro-studio-7579831.jpg"
+import pharmacistImage from "./passion-images/pexels-edward-jenner-4031417.jpg"
 
 const configPassionCards = [
   {
@@ -34,22 +34,24 @@ const configPassionCards = [
     ],
     image: pharmacistImage,
   },
-];
+]
 
 function Passion() {
   return (
     <section className={style["passion-section"]}>
       <div className={style["main"]}>
-        <Carousel className={style["carusel"]}>
+        <Carousel fade className={style["carusel"]}>
           {configPassionCards.map((data) => {
-            return <Carousel.Item>
-            <PassionCard data={data}/>
-          </Carousel.Item>;
+            return (
+              <Carousel.Item>
+                <PassionCard data={data} />
+              </Carousel.Item>
+            )
           })}
         </Carousel>
       </div>
     </section>
-  );
+  )
 }
 
-export default Passion;
+export default Passion
