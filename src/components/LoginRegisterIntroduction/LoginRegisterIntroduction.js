f31cffaa074ecb721a3d20d1b3e7144d87d77590
Warw1ck <bradvata15@gmail.com>
2023-12-09 22:02:58 +0200

    Add a comment

--- src/components/LoginRegisterIntroduction/LoginRegisterIntroduction.js
+++ src/components/LoginRegisterIntroduction/LoginRegisterIntroduction.js
@@ -3,7 +3,7 @@ import "./LoginRegister.css";
 import LoginPopUpComponent from "./LoginRegisterPopUp/LoginRegisterPopUpComponent";
 
 const LoginRegisterPage = () => {
-    const [formName, setForm] = useState('register')
+    const [formName, setForm] = useState('login') // the formName should be one of the three login register or null
     return (
     <>
       <div className="page-pop-up">
