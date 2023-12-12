390f6fd849829aa06a997ba51d0a56c1da32169e
Warw1ck <bradvata15@gmail.com>
2023-12-12 20:27:02 +0200

    Fix more comments from pull request

--- src/components/AuthenticationPage/AuthenticationContent/RegisterForm/RegisterForm.js
+++ src/components/AuthenticationPage/AuthenticationContent/RegisterForm/RegisterForm.js
@@ -4,19 +4,19 @@ import CredentialsFieldsComponent from "./BaseFields/CredentialsFieldsComponent"
 import PersonalFieldsComponent from "./BaseFields/PersonalFieldsComponent";
 
 const RegisterForm = () => {
-  const [numberPart, setNumberPart] = useState(1)
+  const [registrationStep, setRegistrationStep] = useState(1)
   const [profile, setProfile] = useState("patient");
 
   return (
     <>
       <form>
         <h3>Register</h3>
-        <div className={numberPart!==1 && 'hideFields'}>
+        <div className={registrationStep!==1 && 'hideFields'}>
           <CredentialsFieldsComponent setProfile={setProfile}/>
-          <button type="button" className="fadeIn fourth popup-form-button-next popup-form-button"  onClick={()=>setNumberPart(2)}>Next</button>
+          <button type="button" className="fadeIn fourth popup-form-button-next popup-form-button"  onClick={()=>setRegistrationStep(2)}>Next</button>
           
         </div>
-        <div className={numberPart!==2 && 'hideFields'}>
+        <div className={registrationStep!==2 && 'hideFields'}>
           <PersonalFieldsComponent profile={profile}/>
           <button type="submit" className="fadeIn fourth popup-form-button" >Register</button>
         </div>
