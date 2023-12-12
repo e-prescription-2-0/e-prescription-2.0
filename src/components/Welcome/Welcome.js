82e9d891a9b4c57c0e7f7a2e7db85109f41f9b6b
Luben Sirakov <lubo95sirakov@gmail.com>
2023-12-12 20:09:35 +0200

    code clean up

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,7 +1,3 @@
-import { useState } from "react"
-import { useReduxAction } from "../../hooks/useReduxAction"
-import { useReduxState } from "../../hooks/useReduxState"
-import { usersSlice } from "../../reducers/users"
 import Benefits from "./Benefits/Benefits"
 import DemoSteps from "./DemoSteps/DemoSteps"
 import Hero from "./Hero/Hero"
@@ -9,54 +5,10 @@ import Passion from "./Passion/Passion"
 import style from "./Welcome.module.css"
 
 export const Welcome = () => {
-  const [inputValue, setInputValue] = useState("1")
-  // const [currentInput, setCurrentInput] = useState("1")
-  // const getAllMedicines = useReduxAction(medicinesSlice.actions.fetchMedicines)
-  const getAllDoctors = useReduxAction(usersSlice.actions.fetchDoctors)
-  const getDoctorById = useReduxAction(usersSlice.actions.fetchDoctorById)
-  // const medicines = useReduxState((state) => state.medicines.allMedicines)
-  const doctors = useReduxState((state) => state.users.doctors)
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    // Do something with the input value, for now, let's just log it
-    console.log("Input Value:", inputValue)
-  }
-
   return (
     <div className={style["main"]}>
       <Hero />
       <Passion />
-
-      {/* <form onSubmit={handleSubmit}>
-        <label>
-          Type something:
-          <input type="text" value={inputValue} onChange={handleChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-
-      <button
-        style={{ width: 50, height: 20, fontSize: 32 }}
-        onClick={() => getDoctorById(inputValue)}
-      >
-        Click
-      </button>
-
-      <div style={{ marginTop: 10 }}>
-        {doctors.map((d) => {
-          return (
-            <>
-              <p>{d.name}</p>
-              <p>{d.hospitalName}</p>
-            </>
-          )
-        })}
-      </div> */}
 
       <DemoSteps />
 
