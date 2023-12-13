3877e881bf8f16e9ae91d96187f3cc1692105abd
Warw1ck <bradvata15@gmail.com>
2023-12-13 21:39:27 +0200

    Fix login style

--- src/components/AuthenticationPage/AuthenticationContent/RegisterForm/RegisterForm.js
+++ src/components/AuthenticationPage/AuthenticationContent/RegisterForm/RegisterForm.js
@@ -1,45 +1,46 @@
-import { useCallback, useState } from "react"
-import CredentialsFieldsComponent from "./BaseFields/CredentialsFieldsComponent"
-import PersonalFieldsComponent from "./BaseFields/PersonalFieldsComponent"
+import { useCallback, useState } from "react";
+import CredentialsFieldsComponent from "./BaseFields/CredentialsFieldsComponent";
+import PersonalFieldsComponent from "./BaseFields/PersonalFieldsComponent";
 
 const RegisterForm = () => {
-  const [registrationStep, setRegistrationStep] = useState(1)
-  const [profile, setProfile] = useState("patient")
+  const [registrationStep, setRegistrationStep] = useState(1);
+  const [profile, setProfile] = useState("patient");
 
   const renderRegistrationStep = useCallback(() => {
-    if (registrationStep === 1) {
-      return (
-        <>
-          <CredentialsFieldsComponent setProfile={setProfile} />
-          <button
-            type="button"
-            className="fadeIn fourth popup-form-button-next popup-form-button"
-            onClick={() => setRegistrationStep(2)}
-          >
-            Next
-          </button>
-        </>
-      )
-    } else if (registrationStep === 2) {
-      return (
-        <>
-          <PersonalFieldsComponent profile={profile} />
-          <div>
+    switch (registrationStep) {
+      case 1:
+        return (
+          <>
+            <CredentialsFieldsComponent setProfile={setProfile} />
             <button
               type="button"
-              className="fadeIn fourth popup-form-button"
-              onClick={() => setRegistrationStep(1)}
+              className="fadeIn fourth popup-form-button-next popup-form-button"
+              onClick={() => setRegistrationStep(2)}
             >
-              Back
+              Next
             </button>
-            <button type="button" className="fadeIn fourth popup-form-button">
-              Register
-            </button>
-          </div>
-        </>
-      )
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <PersonalFieldsComponent profile={profile} />
+            <div className="popup-form-button-holder">
+              <button
+                type="button"
+                className="fadeIn fourth popup-form-button popup-form-button-back"
+                onClick={() => setRegistrationStep(1)}
+              >
+                Back
+              </button>
+              <button type="button" className="fadeIn fourth popup-form-button">
+                Register
+              </button>
+            </div>
+          </>
+        );
     }
-  }, [registrationStep, setRegistrationStep])
+  }, [registrationStep, setRegistrationStep]);
 
   return (
     <>
@@ -49,7 +50,7 @@ const RegisterForm = () => {
         {renderRegistrationStep()}
       </form>
     </>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
