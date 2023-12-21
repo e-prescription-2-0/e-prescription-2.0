6d9d0b3fc9fa99f0b9a318e5335438d80b288b68
Warw1ck <bradvata15@gmail.com>
2023-12-21 19:55:34 +0200

    Create get email address step in forgot password

--- src/components/AuthenticationPage/AuthenticationContent/ForgotPasswordForm/ForgotPasswordForm.js
+++ src/components/AuthenticationPage/AuthenticationContent/ForgotPasswordForm/ForgotPasswordForm.js
@@ -1,16 +1,33 @@
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import style from "../../AuthenticationPage.module.css";
+import FieldBuilder from "../helpers/FieldBuilder";
+import { useState } from "react";
+import GetEmailAddress from "./ForgotPasswordSteps/GetEmailAddress";
 
 const ForgotPasswordForm = () => {
+  const [changeForgotPasswordFormData, setForgotPasswordFormData] = useState(
+    {}
+  );
+
+  const handleEmailChange = (e) => {
+    const { name, value } = e.target.value;
+
+    setForgotPasswordFormData({
+      ...changeForgotPasswordFormData,
+      [name]: value,
+    });
+  };
+
 
 
   return (
     <>
-      <Form>
-        ForgotPasswordForm
+      <Form className={style["form-forgot-password"]}>
+        <GetEmailAddress handleEmailChange={handleEmailChange} setForgotPasswordFormData={setForgotPasswordFormData}/>
 
       </Form>
     </>
   );
 };
 
-export default ForgotPasswordForm
+export default ForgotPasswordForm;
