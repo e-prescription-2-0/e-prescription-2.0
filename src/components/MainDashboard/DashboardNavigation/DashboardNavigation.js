86f7c31d53d4de9cf6f0186d42835d823c97ea7b
Warw1ck <bradvata15@gmail.com>
2023-12-23 17:15:47 +0200

    Update Side navigation and make it responcive

--- src/components/MainDashboard/DashboardNavigation/DashboardNavigation.js
+++ src/components/MainDashboard/DashboardNavigation/DashboardNavigation.js
@@ -1,22 +1,30 @@
-import style from './DashboardNavigation.module.css'
-import { dashBoardNavData } from '../../../constants/dashBoardNavData';
-
+import style from "./DashboardNavigation.module.css";
+import { dashBoardNavData } from "../../../constants/dashBoardNavData";
 
 import DashboardNavigationItem from "./DashboardNavigationItem";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTableColumns } from "@fortawesome/free-solid-svg-icons";
 
 const DashboardNavigation = () => {
-
-    const role = 'doctor' // coming and depend of redux/context state 
- 
+  const role = "doctor"; // coming and depend of redux/context state
 
   return (
-    <aside className={style['aside-navigation-container']}>
-      <nav>
-        <ul className={style['aside-nav-list']}>
-            {dashBoardNavData[role].map((x,index) => <DashboardNavigationItem key={index}{...x}/>)}
-        </ul>
-      </nav>
-    </aside>
+    <Navbar expand="lg" className={style["aside-navigation-container"]}>
+      <Container className={style["boostrap-container-div-container"]}>
+        <Navbar.Brand id={style["aside-navbar-brand"]} href="#home">
+          Services
+        </Navbar.Brand>
+        <Navbar.Toggle id={style["aside-navbar-toggle"]} aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className={style["aside-nav-list"]}>
+            {dashBoardNavData[role].map((x, index) => (
+              <DashboardNavigationItem key={index} {...x} />
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/components/MainDashboard/DashboardNavigation/DashboardNavigationItem.js
+++ src/components/MainDashboard/DashboardNavigation/DashboardNavigationItem.js
@@ -1,11 +1,12 @@
+import { Nav } from "react-bootstrap";
 import style from "./DashboardNavigation.module.css";
 
 const DashboardNavigationItem = ({ icon, link }) => {
   return (
-    <li className={style['aside-nav-list-item']}>
+    <Nav.Link className={style['aside-nav-list-item']}>
       {icon}
       {link}
-    </li>
+    </Nav.Link>
   );
 };
 
