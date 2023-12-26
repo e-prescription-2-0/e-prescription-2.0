73f5fb405a23c51945e2b4d16447d83f76aec3c3
Warw1ck <bradvata15@gmail.com>
2023-12-26 19:20:07 +0200

    Create Search Doctor Page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header/Header"
 import MainDashboard from "./components/MainDashboard/MainDashboard"
 import { Welcome } from "./components/Welcome/Welcome"
 import store from "./redux"
+import SearchDoctors from "./components/SearchDoctors/SearchDoctors"
 
 const App = () => {
   return (
@@ -16,6 +17,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/dashboard" element={<MainDashboard />} />
+          <Route path="/search-doctors" element={<SearchDoctors/>} />
+          
         </Routes>
       </Provider>
 
---
+++ src/components/SearchDoctors/SearchDoctors.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Container, InputGroup, FormControl, ListGroup } from "react-bootstrap";
+import DashboardNavigation from "../MainDashboard/DashboardNavigation/DashboardNavigation";
+import style from "./SearchDoctors.module.css"
+
+
+const SearchDoctors = () => {
+  return (
+    <section className={style["main-search-doctors-section"]}>
+      <DashboardNavigation />
+      <Container className={style["main-search-doctors-container"]}>
+        <h1 className="text-center">Търси Доктори</h1>
+        <InputGroup className="mb-3">
+          <FormControl
+            id="filterInput"
+            placeholder="Search names..."
+            aria-label="Search names"
+            aria-describedby="basic-addon2"
+          />
+        </InputGroup>
+        <ListGroup as="ul" className="collection with-header">
+          <ListGroup.Item as="li" className="collection-header">
+            <h5>A</h5>
+          </ListGroup.Item>
+          <ListGroup.Item as="li" className="collection-item">
+            <a href="#">Abe</a>
+          </ListGroup.Item>
+          <ListGroup.Item as="li" className="collection-item">
+            <a href="#">Adam</a>
+          </ListGroup.Item>
+          <ListGroup.Item as="li" className="collection-item">
+            <a href="#">Alen</a>
+          </ListGroup.Item>
+          <ListGroup.Item as="li" className="collection-item">
+            <a href="#">Anna</a>
+          </ListGroup.Item>
+          <ListGroup.Item as="li" className="collection-header">
+            <h5>B</h5>
+          </ListGroup.Item>
+          <ListGroup.Item as="li" className="collection-item">
+            <a href="#">Anna</a>
+          </ListGroup.Item>
+          {/* ... (Repeat the pattern for other letters and names) */}
+        </ListGroup>
+      </Container>
+    </section>
+  );
+};
+
+export default SearchDoctors;
