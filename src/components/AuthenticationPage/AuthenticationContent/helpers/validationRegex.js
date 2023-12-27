c1bc560abda91a8f64d1f37c10d86c1e251ba011
Warw1ck <bradvata15@gmail.com>
2023-12-27 14:01:32 +0200

    Update validation for password and names, forgot password link hover

--- src/components/AuthenticationPage/AuthenticationContent/helpers/validationRegex.js
+++ src/components/AuthenticationPage/AuthenticationContent/helpers/validationRegex.js
@@ -1,30 +1,36 @@
 export const validationRegex = {
   email: {
-    validation: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    validation: /^(?=.*[A-Za-zА-Яа-я])(?=.*\d?).{8,}$/,
     errorMessage: "Email is not a valid",
   },
   password: {
-    validation: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    validation: /^(?=.*[A-Za-zА-Яа-я])(?=.*\d?).{8,}$/,
     errorMessage: "Password should be at least 8 characters and digits",
   },
 
   repeatPassword: {
-    validation: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    validation: /^(?=.*[A-Za-zА-Яа-я])[A-Za-zА-Яа-я\d]{8,}$/,
     errorMessage: "Passwords do not match",
   },
   firstName: {
-    validation: /^[a-zA-Z]{3,}$/,
+    validation: /^[A-Za-zА-Яа-я]{2,}$/,
     errorMessage: "First name is not a valid",
   },
   lastName: {
-    validation: /^[a-zA-Z]{3,}$/,
+    validation: /^[A-Za-zА-Яа-я]{2,}$/,
     errorMessage: "Last name is not a valid",
   },
 
   dateOfBirth: { validation: /.+/, errorMessage: "Date of birth is required" },
-  patientId: { validation: /^\d{10}$/, errorMessage: "Identification number is not valid" },
+  patientId: {
+    validation: /^\d{10}$/,
+    errorMessage: "Identification number is not valid",
+  },
 
-  pharmacistId: { validation: /^\d{10}$/, errorMessage: "Identification number is not valid" },
+  pharmacistId: {
+    validation: /^\d{10}$/,
+    errorMessage: "Identification number is not valid",
+  },
   pharmacyName: {
     validation: /^[a-zA-Z]{3,}$/,
     errorMessage: "Pharmacy name is not a valid",
@@ -34,9 +40,12 @@ export const validationRegex = {
     validation: /^[a-zA-Z]{3,}$/,
     errorMessage: "Hospital name is not a valid",
   },
-  doctorId: { validation: /^\d{10}$/, errorMessage: "Identification number is not valid" },
+  doctorId: {
+    validation: /^\d{10}$/,
+    errorMessage: "Identification number is not valid",
+  },
   specialty: {
     validation: /^[a-zA-Z]{3,}$/,
     errorMessage: "Specialty is not a valid",
   },
-}
+};
