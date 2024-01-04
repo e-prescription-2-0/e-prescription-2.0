c6a9ba18eab19a393b5a57fcdd1fb7a0d9996b3d
Ali <alimusli7777@gmail.com>
2024-01-04 21:13:26 +0200

    .

--- src/components/UserProfile/ProfileInfo/ProfileInfo.js
+++ src/components/UserProfile/ProfileInfo/ProfileInfo.js
@@ -4,14 +4,15 @@ const ProfileInfo = ({
     userInfo
 }) => {
     let info = Object.entries(userInfo);
-    
+
     return (
         <>
             {
-                info.map((user) => {
+                info.map((user, index) => {
+                    console.log(index);
                     return (
                         user[1] !== "" &&
-                        <div className={style["content-item"]}>
+                        <div className={ index % 2 === 0? style["content-item"] : style['content-item-bold']}>
                             <span>{user[0]}</span>
                             <p>{user[1]}</p>
                         </div>
