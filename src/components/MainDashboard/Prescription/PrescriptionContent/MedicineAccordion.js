0e18a8613c35a82e4a2568d39724431ed13a5a46
Warw1ck <bradvata15@gmail.com>
2024-01-21 15:42:57 +0200

    Add checkboxses for medicals and paritial button prescription

--- src/components/MainDashboard/Prescription/PrescriptionContent/MedicineAccordion.js
+++ src/components/MainDashboard/Prescription/PrescriptionContent/MedicineAccordion.js
@@ -8,15 +8,22 @@ const MedicineAccordion = ({ medicine, index }) => {
       className={style["div-articles-accordion-item"]}
     >
       <Accordion.Header>
-        <span className={style["div-articles-accordion-item-span"]}>
-          Rp/
-        </span>
-        {medicine.medicineName}
+        <span className={style["div-articles-accordion-item-span"]}>Rp/</span>
+        <div className={style["checkboxes__item"]}>
+          <label className={[style["checkbox"], style["style-f"]].join(" ")}>
+            <input type="checkbox" />
+            <div className={style["checkbox__checkmark"]}></div>
+            <div className={style["checkbox__body"]}>
+              {" "}
+              {medicine.medicineName}
+            </div>
+          </label>
+        </div>
       </Accordion.Header>
       <Accordion.Body className={style["div-articles-accordion-item-content"]}>
         Начин на употреба? {medicine.signature}
       </Accordion.Body>
     </Accordion.Item>
   );
-}
-export default MedicineAccordion
+};
+export default MedicineAccordion;
--- src/components/MainDashboard/Prescription/PrescriptionContent/PrescriptionFooter.js
+++ src/components/MainDashboard/Prescription/PrescriptionContent/PrescriptionFooter.js
@@ -3,10 +3,13 @@ import { useReduxState } from "../../../../hooks/useReduxState";
 import { prescriptionsSlice } from "../../../../reducers/prescriptions";
 import style from "../Prescription.module.css";
 
-
 const PrescriptionFooter = () => {
-  const completePrescription = useReduxAction(prescriptionsSlice.actions.completePrescription)
-  const deletePrescription = useReduxAction(prescriptionsSlice.actions.deletePrescription)
+  const completePrescription = useReduxAction(
+    prescriptionsSlice.actions.completePrescription
+  );
+  const deletePrescription = useReduxAction(
+    prescriptionsSlice.actions.deletePrescription
+  );
 
   const currentPrescription = useReduxState(
     (state) => state.prescriptions.prescription
@@ -14,26 +17,28 @@ const PrescriptionFooter = () => {
 
   let user = {
     role: "pharmacist",
-    id: "658f0b9d1a1925a19548cc8e"
-  }
+    id: "658f0b9d1a1925a19548cc8e",
+  };
 
   const onCompleteBtnClick = () => {
-    completePrescription(currentPrescription._id)
-  }
+    completePrescription(currentPrescription._id);
+  };
 
   const onDeleteBtnClick = () => {
-    let res = window.confirm("Are you sure you want to delete this prescription?")
+    let res = window.confirm(
+      "Are you sure you want to delete this prescription?"
+    );
 
-    if(res){
-      deletePrescription(currentPrescription._id)
+    if (res) {
+      deletePrescription(currentPrescription._id);
     }
-  }
+  };
 
   const isOwner = () => {
     //when we have saved user in redux, take the user from there
-    
-    return user.id === currentPrescription.prescribedBy._id
-  }
+
+    return user.id === currentPrescription.prescribedBy._id;
+  };
 
   return (
     <div className={style["div-userInfo"]}>
@@ -48,25 +53,43 @@ const PrescriptionFooter = () => {
         <p>Пол: {currentPrescription.prescribedTo.gender}</p>
       </div>
 
-        <div className={style["center-div"]}>
-          {
-            user.role === "pharmacist" &&
-            <div className={style["center-div"]}>
-              <p>Изпълнена рецепта?</p>
-              {
-                currentPrescription.isCompleted ? <i id={style["prescription-completed"]} className="fa-solid fa-check"></i> : <button onClick={onCompleteBtnClick} className={style["isCompleted-btn-yes"]}>Изпълни</button>
-              }
-            </div>
-          }
-          {
-            user.role === "doctor" && isOwner() &&
-            <div className={style["center-div"]}>
-              <button onClick={onDeleteBtnClick} className={style["delete-btn"]}>Delete</button>
-            </div>
-          }
-
-        </div>
-
+      <div className={style["center-div"]}>
+        {user.role === "pharmacist" && (
+          <div className={style["center-div"]}>
+            {currentPrescription.isCompleted ? (
+              <i
+                id={style["prescription-completed"]}
+                className="fa-solid fa-check"
+              ></i>
+            ) : (
+              <>
+                <p>Изпълни рецепта?</p>
+                <div>
+                  <button
+                    onClick={onCompleteBtnClick}
+                    className={style["isCompleted-btn-yes"]}
+                  >
+                    Частично
+                  </button>
+                  <button
+                    onClick={onCompleteBtnClick}
+                    className={style["isCompleted-btn-yes"]}
+                  >
+                    Напълно
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        )}
+        {user.role === "doctor" && isOwner() && (
+          <div className={style["center-div"]}>
+            <button onClick={onDeleteBtnClick} className={style["delete-btn"]}>
+              Delete
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
