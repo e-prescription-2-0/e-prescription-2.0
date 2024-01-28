37a5168e131901721562caa00f609a53d105cf40
Warw1ck <bradvata15@gmail.com>
2024-01-28 16:49:09 +0200

    Update dashboard page style hight of the elements

--- src/components/MainDashboard/PrescriptionsList/PrescriptionsList.js
+++ src/components/MainDashboard/PrescriptionsList/PrescriptionsList.js
@@ -1,44 +1,44 @@
-import { useCallback, useEffect, useState } from "react"
-import { useMediaQuery } from "react-responsive"
-import { useReduxAction } from "../../../hooks/useReduxAction"
-import { useReduxState } from "../../../hooks/useReduxState"
-import { prescriptionsSlice } from "../../../reducers/prescriptions"
-import LoadingCircle from "../../Loadings/LoadingCircle/LoadingCircle"
-import DesktopPrescriptionsList from "./DekstopPrescriptionList"
-import EmptyPrescriptionsList from "./EmptyPrescriptionsList"
-import MobilePrescriptionsList from "./MobilePrescriptionsList"
+import { useCallback, useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { useReduxAction } from "../../../hooks/useReduxAction";
+import { useReduxState } from "../../../hooks/useReduxState";
+import { prescriptionsSlice } from "../../../reducers/prescriptions";
+import LoadingCircle from "../../Loadings/LoadingCircle/LoadingCircle";
+import DesktopPrescriptionsList from "./DekstopPrescriptionList";
+import EmptyPrescriptionsList from "./EmptyPrescriptionsList";
+import MobilePrescriptionsList from "./MobilePrescriptionsList";
 
 const PrescriptionsList = ({}) => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-  const isDesktop = useMediaQuery({ minWidth: 1501 })
+  const isDesktop = useMediaQuery({ minWidth: 1501 });
 
   const getPrescriptions = useReduxAction(
     prescriptionsSlice.actions.fetchMyPrescriptions
-  )
+  );
 
   const prescriptions = useReduxState(
     (state) => state.prescriptions.allMyPrescriptions
-  )
+  );
 
   const prescriptionList = useCallback(() => {
     if (prescriptions.length === 0) {
-      return <EmptyPrescriptionsList />
+      return <EmptyPrescriptionsList />;
     }
     if (!isDesktop) {
-      return <MobilePrescriptionsList prescriptions={prescriptions} />
+      return <MobilePrescriptionsList prescriptions={prescriptions} />;
     } else {
-      return <DesktopPrescriptionsList prescriptions={prescriptions} />
+      return <DesktopPrescriptionsList prescriptions={prescriptions} />;
     }
-  }, [prescriptions, isDesktop])
+  }, [prescriptions, isDesktop]);
 
   useEffect(() => {
     if (loading) {
-      getPrescriptions()
-      setLoading(false)
+      getPrescriptions();
+      setLoading(false);
     }
-  }, [getPrescriptions, loading])
+  }, [getPrescriptions, loading]);
 
-  return loading ? <LoadingCircle /> : <div>{prescriptionList()}</div>
-}
-export default PrescriptionsList
+  return loading ? <LoadingCircle /> : prescriptionList();
+};
+export default PrescriptionsList;
