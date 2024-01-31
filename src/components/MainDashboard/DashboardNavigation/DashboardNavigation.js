1b43107be46a57dbb7fbdfe943f2cbc86f453790
Luben Sirakov <lubo95sirakov@gmail.com>
2024-01-31 20:22:36 +0200

    update mobile dashboard navigation

--- src/components/MainDashboard/DashboardNavigation/DashboardNavigation.js
+++ src/components/MainDashboard/DashboardNavigation/DashboardNavigation.js
@@ -8,7 +8,6 @@ import DashboardNavigationItem from "./DashboardNavigationItem"
 const DashboardNavigation = () => {
   const role = "doctor" // coming and depend of redux/context state;
   const currentActiveLink = useReduxState((state) => state.dashboard.activeLink)
-  console.log(currentActiveLink)
 
   return (
     <Navbar expand="xl" className={style["aside-navigation-container"]}>
@@ -20,6 +19,7 @@ const DashboardNavigation = () => {
           id={style["aside-navbar-toggle"]}
           aria-controls="basic-navbar-nav"
         />
+
         <Navbar.Collapse>
           <Nav className={style["aside-nav-list"]}>
             {dashBoardNavData[role]?.map((x, index) => (
