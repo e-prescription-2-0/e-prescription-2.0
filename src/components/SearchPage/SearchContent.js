887fe96789fa70a275f09de66c3cfae903eab61c
Miroslav DImitrov <miro_sv@yahoo.com>
2024-02-02 22:44:19 +0200

    fix search patient list

--- src/components/SearchPage/SearchContent.js
+++ src/components/SearchPage/SearchContent.js
@@ -1,77 +1,74 @@
-import { useState } from "react"
-import { useSearchParams } from "react-router-dom"
-import { useReduxAction } from "../../hooks/useReduxAction"
-import { useReduxState } from "../../hooks/useReduxState"
-import { searchSlice } from "../../reducers/search"
-import BaseSearchFields from "./Fields/BaseSearchField"
-import ListSearchResult from "./ListSearchResult"
-import style from "./SearchPage.module.css"
+import { Container } from "react-bootstrap";
+import style from "./SearchPage.module.css";
+import ListSearchResult from "./ListSearchResult";
+import BaseSearchFields from "./Fields/BaseSearchField";
+import { useSearchParams } from "react-router-dom";
+import { useReduxState } from "../../hooks/useReduxState";
+import { useReduxAction } from "../../hooks/useReduxAction";
+import { searchSlice } from "../../reducers/search";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
-const SearchContent = ({
-  searchType,
-  hidePatientList,
-  isPrescriptionCreateMode,
-}) => {
-  const [searchParams, setSearchParams] = useSearchParams({})
+
+
+const SearchContent = ({ searchType, hidePatientList,isPrescriptionCreateMode }) => {
+  const [searchParams, setSearchParams] = useSearchParams({});
 
   // Redux for all doctors
-  const fetchDoctors = useReduxAction(searchSlice.actions.fetchDoctors)
+  const fetchDoctors = useReduxAction(searchSlice.actions.fetchDoctors);
   const collectionDoctors = useReduxState(
     (state) => state.search.collectionDoctors
-  )
+  );
   // Redux for prescriptions
   const fetchAllPrescriptions = useReduxAction(
     searchSlice.actions.fetchAllPrescriptions
-  )
+  );
   const collectionAllPrescriptions = useReduxState(
     (state) => state.search.collectionAllPrescriptions
-  )
+  );
 
   // Redux for all patients
-  const fetchAllPatients = useReduxAction(searchSlice.actions.fetchAllPatients)
+  const fetchAllPatients = useReduxAction(searchSlice.actions.fetchAllPatients);
   const collectionAllPatients = useReduxState(
     (state) => state.search.collectionAllPatients
-  )
+  );
 
   // Redux for User Patients
-  const fetchMyPatients = useReduxAction(searchSlice.actions.fetchMyPatients)
+  const fetchMyPatients = useReduxAction(searchSlice.actions.fetchMyPatients);
   const collectionMyPatients = useReduxState(
     (state) => state.search.collectionMyPatients
-  )
+  );
 
-  const [isMyPatientsChecked, setIsMyPatientsChecked] = useState(false)
+  const [isMyPatientsChecked, setIsMyPatientsChecked] = useState(false);
 
-  let collection, fetchCollection, titlePage, placeholderText
+  
+  let collection, fetchCollection, titlePage, placeholderText;
 
   switch (searchType) {
     case "doctors":
-      collection = collectionDoctors
-      fetchCollection = fetchDoctors
-      placeholderText = "Търси по имейл на лекар"
-      titlePage = "Търси Доктори"
-      break
+      collection = collectionDoctors;
+      fetchCollection = fetchDoctors;
+      placeholderText = "Търси по имейл на лекар";
+      titlePage = "Търси Доктори";
+      break;
     case "prescriptions":
-      collection = collectionAllPrescriptions
-      fetchCollection = fetchAllPrescriptions
+      collection = collectionAllPrescriptions;
+      fetchCollection = fetchAllPrescriptions;
 
-      placeholderText = "Търси по номер на рецепта"
-      titlePage = "Търси Рецепта"
-      break
+      placeholderText = "Търси по номер на рецепта";
+      titlePage = "Търси Рецепта";
+      break;
     case "patients":
-      if (isMyPatientsChecked) {
-        collection = collectionMyPatients
-        fetchCollection = fetchMyPatients
-      } else {
-        collection = collectionAllPatients
-        fetchCollection = fetchAllPatients
-      }
-      placeholderText = "Търси по ЕГН на пациент"
-      titlePage = "Търси Пациенти"
+        collection = collectionAllPatients;
+        fetchCollection = fetchAllPatients;
+      // }
+      placeholderText = "Търси по ЕГН на пациент";
+      titlePage = "Търси Пациенти";
 
-      break
+      break;
   }
   return (
-    <div className={style["main-search-doctors-container"]}>
+    <Container className={style["main-search-doctors-container"]}>
       <h1 className="text-center">{titlePage}</h1>
 
       <BaseSearchFields
@@ -91,10 +88,13 @@ const SearchContent = ({
         hidePatientList={hidePatientList}
         searchType={searchType}
         isPrescriptionCreateMode={isPrescriptionCreateMode}
-        isMyPatientsChecked={isMyPatientsChecked}
-      />
-    </div>
-  )
-}
+        isMyPatientsChecked = {isMyPatientsChecked}
 
-export default SearchContent
+     
+
+      />
+    </Container>
+  );
+};
+
+export default SearchContent;
